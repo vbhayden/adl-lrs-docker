c5196697c3ecf68f5c8240b3d3d8efd15de581b9
Aaron-onVBH <vbhayden3@gmail.com>
2021-02-11 16:30:43 -0500

    updating nginx structure

--- postgres/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE ROLE "lrs_owner" WITH PASSWORD 'lrs_password';
-
-ALTER ROLE "lrs_owner" WITH LOGIN CREATEDB;
-
-CREATE DATABASE "lrs";
-GRANT ALL PRIVILEGES ON DATABASE "lrs" TO "lrs_owner";
